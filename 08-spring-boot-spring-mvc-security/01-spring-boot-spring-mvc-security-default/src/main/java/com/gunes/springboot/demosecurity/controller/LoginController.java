c85ee9d8140855b949172256163424cc383438c4
gunesserkan <gunesserkan883@gmail.com>
2024-08-04 00:24:54 +0300

    spring mvc security study

---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/gunes/springboot/demosecurity/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.gunes.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/showLoginPage")
+    public String showLoginPage() {
+        return "fancy-login";
+    }
+
+    @GetMapping("/access-denied")
+    public String showAccessDenied() {
+        return "access-denied";
+    }
+}
